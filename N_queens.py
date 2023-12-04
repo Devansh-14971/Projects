d211a6ab6f66b035fabed9c9a319fe303fa814b9
Devansh-14971 <121493585+Devansh-14971@users.noreply.github.com>
2023-12-04 19:28:05 +0530

    Update N_queens.py

--- N_queens.py
+++ N_queens.py
@@ -4,14 +4,38 @@ class board(self):
     def __init__(self,n):
         board = [[0 for i in range(n)] for x in range(n)]
     def update(self,(i,c)):
-    	j,k = i,c
-       	i,c = 0,0
+    	j,k = 0,0
         while i<n:
             self[i][k]=1
             i+=1
         while c<n:
             self[j][c]=1
             c+=1
+        #down right
+        j,k = i,c
+        while(i<n and c<n):
+            self[i][c]=1
+            i+=1
+            c+=1
+        #up right
+        j,k = i,c
+        while(i<n and c<n):
+            self[i][c]=1
+            i+=1
+            c-=1
+        #down left
+        j,k = i,c
+        while(i<n and c<n):
+            self[i][c]=1
+            i-=1
+            c+=1
+        #up left
+        j,k = i,c
+        while(i<n and c<n):
+            self[i][c]=1
+            i-=1
+            c-=1
+            
         
 def Nqueens(i,b,n):
     for c in range(n):
