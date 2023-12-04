39fb7f325b9fca9c66d7164c0d63592dcb7d21ff
Devansh-14971 <121493585+Devansh-14971@users.noreply.github.com>
2023-12-04 19:19:14 +0530

    Update N_queens.py

--- N_queens.py
+++ N_queens.py
@@ -1,29 +1,22 @@
 #solve for N queens
 n = int(input())
 class board(self):
-	def __init__(self,n):
-    	board = [[0 for i in range(n)] for x in range(n)]
+    def __init__(self,n):
+        board = [[0 for i in range(n)] for x in range(n)]
     def update(self,(i,c)):
     	j,k = i,c
-        i,c = 0,0
-    	while i<n:
-        	self[i][k]=1
+       	i,c = 0,0
+        while i<n:
+            self[i][k]=1
             i+=1
         while c<n:
-        	self[j][c]=1
+            self[j][c]=1
             c+=1
         
 def Nqueens(i,b,n):
-	for c in range(n):
-    	if b[i][c]==0:
-        	b[i][c] = q
-            
-    
-    
-    
-    
-    
-    
-    
+    for c in range(n):
+        if b[i][c]==0:
+            b[i][c] = q
+board = board()
 if __name__ == '__main__':
-	Nqueens(0,board,n)
+    Nqueens(0,board,n)
